6dac337f81ca5edfa31a626b1074ca695bda646f
jenilpatel252525-cyber <jenilpatel252525@gmail.com>
2026-06-10 19:21:34 +0530

    Configure CORS

--- myproject/settings.py
+++ myproject/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-     'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,7 +67,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "*",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
